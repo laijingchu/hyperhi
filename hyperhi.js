00547d34cc5b2b6c46a060a2b9099d2140494534
laijingchu <laijing.chu@gmail.com>
2023-12-03 18:14:18 -0800

    done

--- hyperhi.js
+++ hyperhi.js
@@ -1,4 +1,18 @@
 const cursor = document.querySelector("div.cursor")
+const canvasIn = document.querySelector("canvas.in")
+const canvasOut = document.querySelector("canvas.out")
+
+let isMouseDown = false
+
+// variables for mouse position
+let mouseX = 0 
+let mouseY = 0
+
+//variable for the cursor dot position
+let cursorX = 0
+let cursorY = 0
+
+let speed = 0.3
 
 // When I hold the mouse down on the page, make the cursor bigger
 const growCursor = function () {
@@ -13,27 +27,121 @@ const shrinkCursor = function () {
 }
 
 // move the cursor based on coordinates
-const moveCursor = function (x, y) {
-    //update cursor css top/left 
-    cursor.style.left = x + "px"
-    cursor.style.top = y + "px"
+function animate () {
+
+    // the distance between the mouse and the cursor
+    let distX = mouseX - cursorX
+    let distY = mouseY - cursorY
+
+    cursorX = cursorX + (distX * speed) // mouseX 
+    cursorY = cursorY + (distY * speed) // mouseY 
+
+    distX = mouseX - cursorX
+    distY = mouseY - cursorY
+
+     //update cursor css top/left 
+    cursor.style.left = cursorX + "px"
+    cursor.style.top = cursorY + "px"
+
+    requestAnimationFrame(animate);
+
 }
 
+animate()
 
-document.addEventListener("mousedown", function () {
+// set up a canvas
+const setupCanvas = function (canvas) {
+    // const sectionTag = document.querySelector("section")
+    // const h = sectionTag.offsetHeight
+    const w = window.innerWidth
+    const h = window.innerHeight
+
+    //retina-friendly canvas, device dimensions
+    const dpi = window.devicePixelRatio // retina-friendly = 2 ; non retina-friendly = 1
+    canvas.width = w * dpi
+    canvas.height = h * dpi
+
+    // width and height in css itself
+    canvas.style.width = w + "px"
+    canvas.style.height = h + "px"
+
+    // which context are we talking about? 2D? 3D?
+    const context = canvas.getContext("2d")
+
+    if (canvas.classList.contains("out")) {
+        context.fillStyle = "#ffffff"
+        context.strokeStyle = "#000000"
+    } else {
+        context.fillStyle = "#000000"
+        context.strokeStyle = "#ffffff"
+    }
+
+    context.scale(dpi, dpi) // for retina screens
+    context.lineWidth = 80
+    context.lineCap = "round"
+    context.lineJoin = "round"
+
+    context.rect(0, 0, w, h)
+    context.fill()
+
+    context.shadowBlur = 80
+    context.shadowColor = context.strokeStyle
+}
+
+// lets draw based on three things: canvas, x and y
+const moveDraw = function (canvas, x, y) {
+    //get the context of this canvas
+    const context = canvas.getContext("2d")
+    // context.rect(x-30, y-30, 60, 40)
+    // context.fill()
+    if (isMouseDown) {
+        context.lineTo(x, y)
+        context.stroke()
+    }
+}
+
+// lets start to draw, based on the canvas, x and y
+const startDraw = function (canvas, x, y) {
+    const context = canvas.getContext("2d")
+    context.moveTo(x, y)
+
+    // const colors = ["red", "yellow", "blue", "green"]
+    // const randomNum = Math.floor(Math.random() * colors.length)
+
+    // context.strokeStyle = colors[randomNum]
+
+    context.beginPath()
+
+}
+
+setupCanvas(canvasIn)
+setupCanvas(canvasOut)
+
+document.addEventListener("mousedown", function (event) {
     // console.log("mousedown")
+    isMouseDown = true
     growCursor()
+    startDraw(canvasIn, event.pageX, event.pageY)
+    startDraw(canvasOut, event.pageX, event.pageY)
 })
 
 document.addEventListener("mouseup", function () {
     // console.log("mouseup")
+    isMouseDown = false
     shrinkCursor()
 })
 
 document.addEventListener("mousemove", function (event) {
-	// console.log(event)
     // event.pageX -> where we are on the page across
     // event.pageY -> where we are on the page downwards
-    moveCursor(event.pageX, event.pageY)
+    mouseX = event.pageX
+    mouseY = event.pageY
+
+    moveDraw(canvasIn, event.pageX, event.pageY)
+    moveDraw(canvasOut, event.pageX, event.pageY)
 })
 
+window.addEventListener ("resize", function () {
+    setupCanvas(canvasIn)
+    setupCanvas(canvasOut)
+})
